69ebed276f46297d120448c04e7784d8c8b204fb
shaan <skhoslajr@gmail.com>
2019-10-27 14:20:08 -0400

    sklearn data

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-#visualize kmeans algorithm
+# visualize kmeans algorithm
+from celluloid import Camera  # useful tool to visualize matplots
+from sklearn.datasets.samples_generator import make_blobs
+from matplotlib import pyplot as plt
 
 
+def create_data(type="sklearn"):
 
-def run_kmeans()
+    if type == "sklearn":
+        X, y = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+        plt.scatter(X[:, 0], X[:, 1])
+        plt.show()
+        print(y)
 
 
 def main():
-    run_kmeans()
+    create_data()
+
 
 if __name__ == "__main__":
     main()
