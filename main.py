861a278f1797ef0a3347c2927b8cee0028b10b4f
shaan <skhoslajr@gmail.com>
2019-10-27 17:47:14 -0400

    mostly done

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from celluloid import Camera  # useful tool to visualize matplots
 from sklearn.datasets.samples_generator import make_blobs
 from matplotlib import pyplot as plt
 import numpy as np
-import sys
+
+
+COLORS = ["grey", "blue", "green", "purple", "pink"]
 
 
 def create_data(type="sklearn"):
     if type == "sklearn":
-        X, y = make_blobs(n_samples=500, centers=4, cluster_std=0.95)
-        # plt.scatter(X[:, 0], X[:, 1])
-        # plt.draw()
-        # plt.pause(0.001)
-        # input("Press enter to continue: ")
+        X, y = make_blobs(n_samples=1000, centers=4, cluster_std=2)
+        plt.scatter(X[:, 0], X[:, 1])
+        plt.show()
         return X
 
 
@@ -40,24 +40,31 @@ def update_centroids(X, index, num_clusters):
 def run_kmeans(X, num_clusters=4):
     centroids = np.random.uniform(low=np.min(X), high=np.max(X), size=(num_clusters, 2))
     index = find_clusters(X, centroids)
-    camera = Camera(plt.figure())
-    ct = 0
+    (ct, camera) = (0, Camera(plt.figure()))
+
     while(True):
         ct += 1
+        print(ct)
+        old_centroids = centroids
         centroids = update_centroids(X, index, num_clusters)
-        index = find_clusters(X, centroids)
-        plt.scatter(X[:, 0], X[:, 1])
-        plt.scatter(centroids[:, 0], centroids[:, 1], color="red")
-        camera.snap()
-        if ct == 100:
+        if (np.array_equal(centroids, old_centroids)) or (ct == 100):
             break
+        index = find_clusters(X, centroids)
+
+        for i in range(len(X)):
+            plt.scatter(X[i][0], X[i][1], color=COLORS[index[i]])
+        plt.scatter(centroids[:, 0], centroids[:, 1], color="r")
+        camera.snap()
+
     anim = camera.animate(blit=True)
+    anim.save("animation.mp4")
 
 
 def main():
-    X = create_data()  # numpy array of 2d numpy arrays
+    X = create_data()
     run_kmeans(X)
 
 
 if __name__ == "__main__":
     main()
+    print('\a')
