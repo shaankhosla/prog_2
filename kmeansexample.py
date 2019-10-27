dd6f1f5f9bd2c4fd5afd78cb4a8c7340dddc4595
Joshua Bowren <jbowren@miami.edu>
2019-10-27 16:00:53 -0400

    Added kmeans example

---
+++ kmeansexample.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ITERATION_COUNT = 4
+SAMPLE_COUNT = 100
+MEAN_COUNT = 3
+
+data = np.random.randn(SAMPLE_COUNT, 2)
+means = np.random.randn(MEAN_COUNT, 2)
+assigns = np.zeros((SAMPLE_COUNT, MEAN_COUNT))
+
+for i in range(ITERATION_COUNT):
+    dists = np.sum((data[:, :, np.newaxis] - means.transpose()) ** 2.0, axis = 1)
+    assigns = np.argsort(dists, axis = 1)[:, 0]
+
+    for j in range(MEAN_COUNT):
+        means[j, :] = np.mean(data[np.where(assigns == j)], axis = 0)
+
+plt.plot(means[0, 0], means[0, 1], 'rx')
+plt.plot(means[1, 0], means[1, 1], 'gx')
+plt.plot(means[2, 0], means[2, 1], 'bx')
+
+first = data[np.where(assigns == 0)]
+second = data[np.where(assigns == 1)]
+third = data[np.where(assigns == 2)]
+
+plt.plot(first[:, 0], first[:, 1], 'ro')
+plt.plot(second[:, 0], second[:, 1], 'go')
+plt.plot(third[:, 0], third[:, 1], 'bo')
+
+plt.show()
+
